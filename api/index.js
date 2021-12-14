f329ef7c927b2a53cf9ef234e7d622d2347b133e
Mark Oliinyk <markoliinyk@gmail.com>
2021-12-14 22:48:05 +0200

    Add new env variable

--- api/index.js
+++ api/index.js
@@ -20,7 +20,7 @@ app.register(cookie, {
 
 const start = async () => {
     try {
-        await app.listen(process.env.PORT || 5000);
+        await app.listen(process.env.MY_PORT || process.env.PORT || 5000);
     } catch (err) {
         app.log.error(err);
         process.exit(1);
